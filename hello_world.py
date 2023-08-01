8cfe876daabf6e9b052f82afce956012b4e9226b
Daniel Barros Da Silva <VIC0143562@aluno.firjansenaisesi.com.br>
2023-08-01 09:25:37 -0300

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,55 @@
+#ingut e uma função que permite perguntar algo ao usario
+# nome = input("Qual é o seu nome?")
+# print("Olá,", nome, ". Tudo bem com Voce?")
+# print(type(nome))
+a = 10
+b = 5.8
+c = "Rio de Janeiro"
+d = True
+
+print("a:", a)
+print("b:", b)
+print("c:", c)
+print("d:", d)
+
+print("Tipo de var a:", type(a)) # tipo inteiro (números inteiros)
+print("Tipo de var b:", type(b)) # tipo inteiro tipo float (números reais)
+print("Tipo de var c:", type(c)) # tipo string (alfanumérico)
+print("Tipo de var d:", type(d)) # tipo boolean (true ou false)
+
+a = 20
+print("a:",a)
+b = "São Paulo"
+print("b:",b)
+print("Tipo da var a:",  type(a))
+print("Tipo da var b:",  type(b))
+
+a = input("Informe um numero: ")
+b = input("Informe outro numero: ")
+print("a: ", a, " - b:",b)
+print("Tipo da var a:",  type(a))
+print("Tipo da var b: ", type(b))
+c = a + b # concatenou as strigns de A e B
+print("c: ",c)
+print("Tipo da var c:", type(c))
+d= int(a)
+print("d: ", d)
+print("Tipo da var a:",  type(d))
+
+a = int(input("Informe um numero: "))
+b = int(input("Informe outro numero: "))
+print("a: ", a, " - b:",b)
+print("Tipo da var a:",  type(a))
+print("Tipo da var b: ", type(b))
+c = a + b
+print("c: ",c)
+print("Tipo da var c:", type(c))
+
+
+
+
+
+
+
+
+
